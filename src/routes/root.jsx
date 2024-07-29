a02a7d3f8db499c2882058500c66663279a63c77
zzik <edin.base@outlook.com>
2024-07-29 22:12:32 +0200

    refactoring root route

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,44 +1,21 @@
 import { Link, useLoaderData } from "react-router-dom";
 import Item from "../components/Item";
-import { getProducts } from "../products";
-import axios from "axios";
+import { getProducts, handleDelete } from "../utils";
 
 export default function Root() {
   const { products } = useLoaderData();
 
-  const handleDelete = async () => {
-    try {
-      let data = document.querySelectorAll("input:checked");
-      data = Array.from(data).map((el) => el.name);
-
-      await axios.post(
-        "http://localhost/tried/delete.php",
-        { skus: data },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      window.location = 'http://localhost:5173';
-    } catch (error) {
-      console.error("Error deleting products:", error);
-    }
-  };
-
   return (
     <>
       <Link to={"/add"}>ADD ITEM</Link>
-      <form onSubmit={handleDelete}>
-        <ol>
-          {products.map((product) => (
-            <li key={product.sku}>
-              <Item product={product} />
-            </li>
-          ))}
-        </ol>
-        <button type="submit">MASS DELETE</button>
-      </form>
+      <button onClick={handleDelete}>MASS DELETE</button>
+      <ol>
+        {products.map((product) => (
+          <li key={product.sku}>
+            <Item product={product} />
+          </li>
+        ))}
+      </ol>
     </>
   );
 }

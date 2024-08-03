ded6b92511008bc9d0f05ee0a4e3544e69dc6211
zzik <edin.base@outlook.com>
2024-08-03 09:56:38 +0200

    go

--- src/components/buttons/Delete.jsx
+++ src/components/buttons/Delete.jsx
@@ -2,7 +2,7 @@
 import { handleDelete } from '../../utils'
 
 const Delete = () => {
-  const addRoute = () => window.location.href = 'http://localhost:5173/add-product'
+  const addRoute = () => window.location.href = 'https://zzik-scandiweb.netlify.app/add-product'
   return <>
     {/* <NavLink to={"/add-product"}>ADD</NavLink> */}
     <button onClick={addRoute}>ADD</button>

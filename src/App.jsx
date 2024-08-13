383d0f7df5e142e2722440c3ae225b73e9b54923
zzik <edinbejzik@gmail.com>
2024-08-13 08:24:36 +0200

    oups

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import { Header, Footer, Display } from "./components/layout";
 import { PayloadProvider } from "./context/PayloadContext";
 
 export default function App() {
-    const token = "123abc345";
 
-// Set the Authorization header globally
-axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   return (
     <>
       <Header />

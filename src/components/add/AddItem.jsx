50cba74086297d98116aca23db4ed5805c47c8a8
zzik <edinbejzik@gmail.com>
2024-08-13 05:52:10 +0200

    idrk

--- src/components/add/AddItem.jsx
+++ src/components/add/AddItem.jsx
@@ -3,14 +3,14 @@ import { General, Switcher } from "./";
 
 const AddItem = () => {
   return (
-    <div id="product_form" onKeyUp={numericValidation}>
+    <form id="product_form" onKeyUp={numericValidation} onSubmit={(e) => e.preventDefault()}>
       <div className="form__heading" >
         <h3>Welcome To Scandiweb</h3>
         <h4>CRUD Form</h4>
       </div>
       <General />
       <Switcher />
-    </div>
+    </form>
   );
 };
 

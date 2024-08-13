0d886e0a00063167947700ae3ff9a87d41acb35f
zzik <edinbejzik@gmail.com>
2024-08-13 06:00:53 +0200

    fd

--- src/components/buttons/Delete.jsx
+++ src/components/buttons/Delete.jsx
@@ -1,12 +1,14 @@
+import { NavLink } from 'react-router-dom'
 import { massDelete } from '../../utils'
 
 const Delete = () => {
   const addRoute = () => window.location.href = 'https://zzik-scandiweb.netlify.app/add-product'
   return <>
-    <button onClick={addRoute}>ADD</button>
-    <label htmlFor="delete">MASS DELETE</label>
-    <input type='button' onClick={massDelete} name='delete' />
-    {/* <button onClick={massDelete}>MASS DELETE</button> */}
+    <NavLink to={'add-product'}>ADD</NavLink>
+    {/* <button onClick={addRoute}>ADD</button> */}
+    {/* <label htmlFor="delete">MASS DELETE</label>
+    <input type='button' onClick={massDelete} name='delete' /> */}
+    <button onClick={massDelete}>MASS DELETE</button>
   </>
 }
 

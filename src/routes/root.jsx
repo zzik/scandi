23517b94544a64dbff8c41362947f561b49a5ce5
zzik <edin.base@outlook.com>
2024-08-19 23:47:40 +0200

    ss

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,8 +1,12 @@
 import { getProducts } from "../utils";
 import { Header, Footer, Display } from "../components/layout";
 import { PayloadProvider } from "../context/PayloadContext";
+import { useEffect } from "react";
 
 export default function Root() {
+  useEffect(() => {
+    document.title = 'Product List';
+  }, []);
   return (
     <PayloadProvider>
       <Header />

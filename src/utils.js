0811bebfeb2ac5bcf37e4830c0b87bf8a5cd352e
zzik <edin.base@outlook.com>
2024-08-20 09:28:38 +0200

    new domain

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function getProducts() {
-  let products = await axios.get("https://0603148163.mom/list.php");
+  let products = await axios.get("https://www.hello060hello.fun/list.php");
   return products.data;
 }
 
@@ -11,7 +11,7 @@ export const massDelete = async () => {
     data = Array.from(data).map((el) => el.name);
 
     await axios.post(
-      "https://0603148163.mom/delete.php",
+      "https://www.hello060hello.fun/delete.php",
       { skus: data },
       {
         headers: {
@@ -43,7 +43,7 @@ export const testSubmit = async (data) => {
   params.append("length", data.length);
 
   try {
-    await axios.post("https://0603148163.mom/add.php", params, {
+    await axios.post("https://www.hello060hello.fun/add.php", params, {
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
       },
@@ -56,7 +56,7 @@ export const testSubmit = async (data) => {
 export const checkSKU = async (sku) => {
   try {
     let check = await axios.get(
-      "https://0603148163.mom/check.php",
+      "https://www.hello060hello.fun/check.php",
       { params: { sku } },
       {
         headers: {

4635757514b168ec7878ae481400a5020b57e5f9
zzik <edin.base@outlook.com>
2024-08-20 09:38:17 +0200

    tiddle

--- src/components/home/List.jsx
+++ src/components/home/List.jsx
@@ -4,17 +4,16 @@ import { getProducts } from "../../utils";
 import { useEffect, useState } from "react";
 
 const List = () => {
-  // const { products } = useLoaderData();
-  const [products, setProducts] = useState()
-  const handleProducts = (dt) => {
-    console.log(dt)
-    setProducts(dt)
-  }
-  useEffect(() => {
-    const products = getProducts().then((stuff)=>handleProducts(stuff))
-    // console.log(products)
-    // handleProducts(products)
-  }, [])
+  const { products } = useLoaderData();
+  // const [products, setProducts] = useState()
+  // const handleProducts = (dt) => {
+  //   setProducts(dt)
+  // }
+  // useEffect(() => {
+  //   getProducts().then((stuff)=>handleProducts(stuff))
+  //   // console.log(products)
+  //   // handleProducts(products)
+  // }, [])
   return (
     <ul>
       {products && products.map((product) => (
--- src/components/layout/Display.jsx
+++ src/components/layout/Display.jsx
@@ -1,15 +1,18 @@
 import { Outlet, useLocation } from "react-router-dom";
 import { AddItem } from "../add";
-import { List } from '../home'
+import { List } from "../home";
+import { useEffect } from "react";
 
 const Display = () => {
   let location = useLocation();
-  let current = location.pathname === "/" ? <List/> : <AddItem/>;
-  // let buttons = location.pathname === '/' ? <Delete /> : <Save />
+  // let current = location.pathname === "/" ? <List /> : <AddItem />;
+  useEffect(() => {
+    document.title = location.pathname === "/" ? "Product List" : "Product Add"
+  }, [location.pathname]);
   return (
     <div className="display">
-      {current}
-      {/* <Outlet /> */}
+      {/* {current} */}
+      <Outlet />
     </div>
   );
 };
